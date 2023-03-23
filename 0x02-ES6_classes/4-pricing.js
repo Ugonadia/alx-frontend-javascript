47d1af288280c8481bd1a2bbd00ded7465d80903
Ozoeze Ugo <uozoeze@gmail.com>
2023-03-23 23:35:13 +0100

    Create 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from '.\3-currency.js'
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(c) {
+    this._currency = c;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
